c6942aba44b9647f42d9839acd04eeabeb53748d
Jeff Goeders <jeff.goeders@gmail.com>
2020-12-16 21:44:37 -0700

    Add simple_unrolled benchmark

---
+++ lab_scheduling/benchmarks/simple_unrolled/simple.c
@@ -0,0 +1,23 @@
+unsigned short lfsr = 0xACE1u;
+unsigned bit;
+
+unsigned my_rand() {
+    bit = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1;
+    return lfsr = (lfsr >> 1) | (bit << 15);
+}
+
+int main() {
+    int A[100];
+    int i;
+
+    for (i = 0; i < 100; i++) {
+        A[i] = my_rand();
+    }
+
+    int sum = 0;
+    for (i = 0; i < 100; i += 4) {
+        sum += A[i] + A[i + 1] + A[i + 2] + A[i + 3];
+    }
+
+    return sum;
+}
